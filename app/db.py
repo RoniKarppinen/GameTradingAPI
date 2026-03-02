db0bc049f14379bbdcc58e9cc9a7edb85a9b2368
RoniKarppinen <karppinenroni@gmail.com>
2026-03-02 20:11:41 +0200

    fixed the linters for db.py and populate.py

--- app/db.py
+++ app/db.py
@@ -1,5 +1,7 @@
+"""
+Most of the code is modified from the exercises
+"""
 from datetime import datetime
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.engine import Engine
@@ -35,25 +37,33 @@ class Game(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="SET NULL"))
 
     owner = db.relationship("User", back_populates="game")
-    sender_trade = db.relationship("Trade",foreign_keys="Trade.sender_game_id", back_populates="sender_game")
-    receiver_trade = db.relationship("Trade",foreign_keys="Trade.receiver_game_id",back_populates="receiver_game")
+    sender_trade = db.relationship("Trade",foreign_keys="Trade.sender_game_id",
+                                   back_populates="sender_game")
+    receiver_trade = db.relationship("Trade",foreign_keys="Trade.receiver_game_id",
+                                    back_populates="receiver_game")
 
 class Trade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime)
     status = db.Column(db.String(20), default="Pending")
-    sender_game_id = db.Column(db.Integer, db.ForeignKey("game.id", ondelete="SET NULL"))
-    receiver_game_id = db.Column(db.Integer, db.ForeignKey("game.id", ondelete="SET NULL"))
+    sender_game_id = db.Column(db.Integer, db.ForeignKey("game.id",
+                                                         ondelete="SET NULL"))
+    receiver_game_id = db.Column(db.Integer, db.ForeignKey("game.id",
+                                                           ondelete="SET NULL"))
 
-    sender_game = db.relationship("Game", foreign_keys="Trade.sender_game_id", back_populates="sender_trade")
-    receiver_game = db.relationship("Game",foreign_keys ="Trade.receiver_game_id", back_populates="receiver_trade")
+    sender_game = db.relationship("Game", foreign_keys="Trade.sender_game_id",
+                                  back_populates="sender_trade")
+    receiver_game = db.relationship("Game",foreign_keys ="Trade.receiver_game_id",
+                                    back_populates="receiver_trade")
 
-    # Inspiration from https://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
+    # Inspiration from
+    # https://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
     def to_dict(self):
         """
         Description: Convert the Trade object to a dictionary for JSON serialization.
         Inputs: Self (the Trade object).
-        Outputs: A dictionary representation of the Trade object, with datetime fields converted to ISO format.
+        Outputs: A dictionary representation of the Trade object,
+            with datetime fields converted to ISO format.
         Exceptions: None.
         """
         result = {}
@@ -64,7 +74,6 @@ class Trade(db.Model):
             # Convert datetime to ISO.
             if isinstance(value, datetime):
                 value = value.isoformat()
-            
             result[name] = value
         return result
 
@@ -73,8 +82,10 @@ def reset_database():
     with app.app_context():
         db.drop_all()
         db.create_all()
-        
-if __name__ == "__main__":  #Clear the existing database for new population, create the database with models above
+
+# Clear the existing database for new population,
+# create the database with models above
+if __name__ == "__main__":
     reset_database() # pragma: no cover
     #Some test cases to try out ondelete logic
 
